914fd7de58c575e730696d25cf8ba29b6e0d440f
Joao Otavio Turatti Barbosa <joaootavio22017@gmail.com>
2020-05-07 23:38:05 -0300

    Leitura dos dados

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
-#include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
-{
-    cout << "Hello world!" << endl;
+{   double a,b,c,d,e;
+    cout << "Isira sua renda anual com salário: " << endl;
+    cin>>a;
+    cout << "Isira sua renda anual com prestação de serviço: " << endl;
+    cin>>b;
+    cout << "Isira sua renda anual com ganho de capital: " << endl;
+    cin>>c;
+    cout << "Isira seus gastos médicos: " << endl;
+    cin>>d;
+    cout << "Isira seus gastos educacionais: " << endl;
+    cin>>e;
+
     return 0;
 }
