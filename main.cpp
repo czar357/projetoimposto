8b303bdd9031e87f7e0705895fa53b312c3d879b
Joao Otavio Turatti Barbosa <joaootavio22017@gmail.com>
2020-05-08 00:13:04 -0300

    Relatório finalizado

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main()
-{   double a,b,c,d,e,f,g,h;
+{   double a,b,c,d,e,f,g,h,i,j;
 cout<<fixed<<setprecision(2);
     cout << "Isira sua renda anual com salário: " << endl;
     cin>>a;
@@ -23,6 +23,9 @@ cout<<fixed<<setprecision(2);
             f == (a/100)*20;}
     g == b/100*15;
     h==c/100*20;
+    i == d + e;
+    j == ((a+b+c)/100)*30;
+
 
     cout<<"RELATÓRIO DE IMPOSTO DE RENDA"<<endl;
 
@@ -36,5 +39,20 @@ cout<<fixed<<setprecision(2);
       cout<<"Imposto sobre salário: "<<f<<endl;
       cout<<"Imposto sobre serviços: "<<g<<endl;
       cout<<"Imposto sobre ganho de capital: "<<h<<endl;
+
+      cout<<"DEDUÇÕES"<<endl;
+      cout<<"Máximo dedutível: "<< j<<endl;
+      cout<<"Gastos dedutíveis: "<<i<<endl;
+
+      cout<<"RESUMO"<<endl;
+      cout<<"Imposto bruto total: "<<a+b+c<<endl;
+      if(i>j){
+        cout<<"Abatimento: "<<j<<endl;
+         cout<<"Imposto devido: "<<(a+b+c)-j<<endl;
+        }
+      else{
+        cout<<"Abatimento: "<<i<<endl;}
+        cout<<"Imposto devido: "<<(a+b+c)-i<<endl;
+
     return 0;
 }
